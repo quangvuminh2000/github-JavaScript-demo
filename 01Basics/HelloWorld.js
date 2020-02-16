ca4bd2f8159e477590e55bc57ac121cdb0ddea7f
Quang Henry <quangvm2000@gmail.com>
2020-02-16 19:26:06 +0700

    Add the first basics of JavaScript

---
+++ 01Basics/HelloWorld.js
@@ -0,0 +1,19 @@
+var myArray = [45, 65, 75];
+
+console.log(myArray);
+
+var topElement = myArray.shift();
+
+console.log(myArray);
+
+myArray.unshift(topElement);
+
+console.log(myArray);
+
+var bottomElement = myArray.pop();
+
+console.log(myArray);
+
+myArray.push(bottomElement);
+
+console.log(myArray);
---
+++ 01Basics/TrueFalse.js
@@ -0,0 +1,11 @@
+function isTrue()
+{
+    return false;
+}
+
+function printCondition()
+{
+    console.log(isTrue());
+}
+
+printCondition();
