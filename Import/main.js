cda86e5e0ccfa1aedeca0fe2c8c82871cfe6947d
Quang Henry <quangvm2000@gmail.com>
2020-02-18 22:12:35 +0700

    Add Import folder

---
+++ Import/main.js
@@ -0,0 +1,7 @@
+// import and export helps to use the function 
+// and code from another file
+import  capitalizeString  from './string_function';
+
+const upperCaseStr = capitalizeString('hello');
+
+console.log(upperCaseStr);
